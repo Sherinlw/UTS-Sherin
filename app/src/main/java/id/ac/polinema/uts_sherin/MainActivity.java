c866ec1735f8fa8efec40cac3c12e01a7093e001
Sherinlw <Sherinlw1410@gmail.com>
2020-04-02 11:51:14 -0700

    Layout

---
+++ app/src/main/java/id/ac/polinema/uts_sherin/MainActivity.java
@@ -0,0 +1,14 @@
+package id.ac.polinema.uts_sherin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
